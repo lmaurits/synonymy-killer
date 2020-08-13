a927b0f32617080965ad958dd58c3b87f78825fd
Luke Maurits <luke@maurits.id.au>
2020-08-13 16:30:41 +0200

    Import and adapt cognate maximising/minimising synonym resolution code from TIGER paper.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import collections
 import random
 import statistics
 import sys
@@ -74,16 +75,83 @@ def kill_random(dataset):
         forms_to_keep.add(survivor)
     _write_new_dataset('mydataset', dataset, forms_to_keep)
 
+def kill_minimum_cognates(dataset):
+    return _kill_minimax_cognates(dataset, "min")
+
+def kill_maximum_cognates(dataset):
+    return _kill_minimax_cognates(dataset, "max")
+
+def _kill_minimax_cognates(dataset, mode="min"):
+    languages, meanings, forms, cogmap = parse_form_table(dataset)
+    forms_to_keep = set()
+    cognates = {}
+    for meaning in meanings:
+        cognate_class_counts = collections.Counter()
+        for lang in languages:
+            key = (lang,meaning)
+            cognates[key] = [cogmap.get(f, "?") for f in forms.get(key, "?")]
+            for c in cognates[key]:
+                if c != "?":
+                    cognate_class_counts[c] += 1
+        # Divide languages into easy and hard cases
+        easy_langs = [l for l in languages if len(cognates[(l, meaning)]) < 2]
+        hard_langs = [l for l in languages if l not in easy_langs]
+        # Make easy assignments
+        attested_cognates = set()
+        for lang in easy_langs:
+            key = (lang,meaning)
+            if not cognates[key]:
+                cognates[key] = "?"
+            elif len(cognates[key]) == 1:
+                cognates[key] = cognates[key].pop()
+                attested_cognates.add(cognates[key])
+        # Make hard assignments
+        for lang in hard_langs:
+            key = (lang,meaning)
+            options = [(cognate_class_counts[c], c) for c in cognates[key]]
+            # Sort cognates from rare to common if we want to maximise cognate
+            # class count, or from common to rare if we want to minimise it.
+            options.sort(reverse = mode == "min")
+            # Preferentially assign a cognate which has already been
+            # assigned if we're trying to minimise, or one which has
+            # not if we're trying to maximise.
+            for n, c in options:
+                if (mode == "min" and c in attested_cognates) or (mode == "max" and c not in attested_cognates):
+                    cognates[key] = c
+                    break
+            # Otherwise just pick the most/least frequent cognate.
+            else:
+                cognates[key] = options[0][1]
+            attested_cognates.add(cognates[key])
+        # Translate the cognate class to keep to a random representative form
+        for lang in languages:
+            key = (lang,meaning)
+            if not key in forms:
+                continue
+            if cognates[key] == "?":
+                synonym_list = forms[key]
+                survivor = random.sample(synonym_list, 1)[0]
+                forms_to_keep.add(survivor)
+            else:
+                for f in forms[key]:
+                    if cogmap[f] == cognates[key]:
+                        forms_to_keep.add(f)
+                        break
+
+    _write_new_dataset('mydataset', dataset, forms_to_keep)
+
 def main():
 
     # Parse arguments
     parser = argparse.ArgumentParser(description='Remove synonyms from a CLDF wordlist.')
     parser.add_argument("metadata", help="CLDF metadata file.")
+    parser.add_argument("--maxcog", action="store_true", help="Remove synonyms to maximise cognate class count.")
+    parser.add_argument("--mincog", action="store_true", help="Remove synonyms to minimise cognate class count.")
     parser.add_argument("--report", action="store_true", help="Display wordlist statistics.")
     parser.add_argument("--random", action="store_true", help="Remove synonyms at random.")
     args = parser.parse_args()
 
-    actions = [args.report, args.random]
+    actions = [args.maxcog, args.mincog, args.report, args.random]
     if actions.count(True) > 1:
         print("Can only select one action!")
         sys.exit(1)
@@ -96,6 +164,10 @@ def main():
         report(dataset)
     elif args.random:
         kill_random(dataset)
+    elif args.mincog:
+        kill_minimum_cognates(dataset)
+    elif args.maxcog:
+        kill_maximum_cognates(dataset)
     # Report by default
     else:
         report(dataset)
